02e3b0d597d6388518fc5edd20ab55f57fd2e638
linx <261173157@qq.com>
2018-12-06 17:01:08 +0800

    已完成躲避球

--- sample/Pygame_learning/pygame_learning.py
+++ sample/Pygame_learning/pygame_learning.py
@@ -6,6 +6,9 @@ from pygame.locals import *
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
+        # self.surf = pygame.image.load('jet.png').convert()
+        # self.surf.set_colorkey((255, 255, 255), RLEACCEL)
+        # self.rect = self.surf.get_rect(center=(400, 300))
         self.surf = pygame.Surface((50, 50))
         self.surf.fill((255, 255, 255))
         self.rect = self.surf.get_rect(center=(400, 300))
