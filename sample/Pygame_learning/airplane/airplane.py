8df10432404eeba4d2923d59171fe3fa7e4d14fe
linx <261173157@qq.com>
2018-12-07 17:02:16 +0800

    第二节第1程序

--- sample/Pygame_learning/airplane/airplane.py
+++ sample/Pygame_learning/airplane/airplane.py
@@ -6,7 +6,7 @@ from pygame.locals import *
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
-        self.surf = pygame.image.load('jet.png')
+        self.surf = pygame.image.load('Player.png')
         self.surf.set_colorkey((255, 255, 255), RLEACCEL)
         self.rect = self.surf.get_rect(center=(400, 300))
         # self.surf = pygame.Surface((50, 50))
@@ -15,13 +15,13 @@ class Player(pygame.sprite.Sprite):
 
     def update(self, pressed_keys):
         if pygame.key.get_pressed()[K_UP]:
-            self.rect.move_ip(0, -1)
+            self.rect.move_ip(0, -5)
         if pygame.key.get_pressed()[K_DOWN]:
-            self.rect.move_ip(0, 1)
+            self.rect.move_ip(0, 5)
         if pygame.key.get_pressed()[K_LEFT]:
-            self.rect.move_ip(-1, 0)
+            self.rect.move_ip(-5, 0)
         if pygame.key.get_pressed()[K_RIGHT]:
-            self.rect.move_ip(1, 0)
+            self.rect.move_ip(5, 0)
         if self.rect.left < 0:
             self.rect.left = 0
         elif self.rect.right > 800:
@@ -37,10 +37,10 @@ class Enemy(pygame.sprite.Sprite):
 
     def __init__(self):
         super(Enemy, self).__init__()
-        self.surf = pygame.Surface((20, 10))
-        self.surf.fill((255, 0, 0))
+        self.surf = pygame.image.load('Enemy.png')
+        self.surf.set_colorkey((255, 255, 255), RLEACCEL)
         self.rect = self.surf.get_rect(center=(random.randint(0, 780), 0))
-        self.speed = random.randint(20, 50)
+        self.speed = random.randint(3, 6)
 
     def update(self, *args):
         self.rect.move_ip(0, self.speed)
@@ -58,8 +58,10 @@ screen = pygame.display.set_mode([800, 600])  # 设定一个窗口
 
 Add_enemy = pygame.USEREVENT+1  # 添加敌人自定义事件
 Enemy_move = pygame.USEREVENT+2  # 敌人移动
-pygame.time.set_timer(Add_enemy, 500)  # 设置敌人出现时间
-pygame.time.set_timer(Enemy_move, 100)  # 设置敌人出现时间
+Player_move = pygame.USEREVENT+3  # 本体移动
+pygame.time.set_timer(Add_enemy, 100)  # 设置敌人出现时间
+pygame.time.set_timer(Enemy_move, 10)  # 设置敌人移动刷新时间
+pygame.time.set_timer(Player_move, 10)  # 设置本体移动刷新时间
 
 
 player = Player()
@@ -84,18 +86,15 @@ while running:
         elif event.type == Enemy_move:
             for enemy in enemies:
                 enemy.update()
-    pressed_keys1 = pygame.key.get_pressed()
-    player.update(pressed_keys1)
+        elif event.type == Player_move:
+            pressed_keys1 = pygame.key.get_pressed()
+            player.update(pressed_keys1)
     if pygame.sprite.spritecollideany(player, enemies):
-        enemy.num()
+        Enemy.num()
         player.kill()
         running = False
 
     screen.fill((255, 255, 255))
     for entity in all_sprites:
         screen.blit(entity.surf, entity.rect)
-    pygame.display.flip()  # 显示出来
-
-
-
-
+    pygame.display.update()  # 显示出来
